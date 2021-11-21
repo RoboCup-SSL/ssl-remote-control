db78e77a12e776890ea703061932699667b04197
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-11-21 17:58:46 +0100

    Add maxRobots and yellowCardsDue to status bar

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -2,12 +2,17 @@
 import {ApiController} from '../services/ApiController';
 import {inject, ref} from 'vue';
 
-const online = ref(false);
+const defaultYellowCardsDue: number[] = []
+const online = ref(false)
+const maxRobots = ref(0)
+const yellowCardsDue = ref(defaultYellowCardsDue)
 const api = inject<ApiController>('api')
 let onlineTimer: NodeJS.Timeout
 
 api?.RegisterStateConsumer((s) => {
   online.value = true
+  maxRobots.value = s.maxRobots
+  yellowCardsDue.value = s.yellowCardsDue.sort((a: number, b: number) => a - b)
   if (onlineTimer) {
     clearTimeout(onlineTimer)
   }
@@ -19,8 +24,13 @@ api?.RegisterStateConsumer((s) => {
 
 <template>
   <div class="bar">
-    <div class="online-state" :class="{online: online, offline: !online}">
-
+    <div class="left-bar-element">
+      Yellow card due:
+      <span class="yellow-card-time" v-for="yellowCardDue of yellowCardsDue">{{ Math.round(yellowCardDue) }}s</span>
+    </div>
+    <div class="right-bar-element online-state" :class="{online: online, offline: !online}"/>
+    <div class="right-bar-element">
+      Max robots: <strong>{{ maxRobots }}</strong>
     </div>
   </div>
 </template>
@@ -36,16 +46,32 @@ api?.RegisterStateConsumer((s) => {
   padding: 0.5em;
   text-align: right;
 }
-.online-state {
-  position: relative;
+
+.right-bar-element {
   float: right;
+  margin-left: 1em;
+}
+
+.left-bar-element {
+  float: left;
+  margin-right: 1em;
+}
+
+.yellow-card-time {
+  margin-right: 0.5em;
+  font-weight: bold;
+}
+
+.online-state {
   width: 1em;
   height: 1em;
   border-radius: 1em;
 }
+
 .online-state.online {
   background-color: #42b983;
 }
+
 .online-state.offline {
   background-color: red;
 }
