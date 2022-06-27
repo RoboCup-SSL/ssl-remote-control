675862e7217dbec9d48d26f5407217122f5a2412
Nicolai Ommer <nicolai.ommer@gmail.com>
2022-06-27 22:44:37 +0200

    Add a padding for small device screen width

--- src/App.vue
+++ src/App.vue
@@ -153,4 +153,11 @@ import StatusBar from './components/StatusBar.vue';
   color: #93D5FF;
 }
 
+/* Chromium has a minimum width of 500px, but the Pi display is only 480px, so we need to add a padding here */
+@media (max-width: 500px) {
+  #app {
+    padding-right: 20px;
+  }
+}
+
 </style>
