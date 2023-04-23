841e6e6b681cb48788a6a1e7c4224f27ba16b83a
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-04-23 20:14:06 +0200

    Update generateProto.sh

--- tools/generateProto.sh
+++ tools/generateProto.sh
@@ -2,26 +2,26 @@
 set -euo pipefail
 
 # determine current script directory
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 PROJECT_DIR="${SCRIPT_DIR}/.."
 cd "${PROJECT_DIR}"
 
-PB_VERSION=3.15.8
+PB_VERSION=22.3
 PB_GO_VERSION=$(go list -m all | grep google.golang.org/protobuf | awk '{print $2}')
 
 # Create a local bin folder
-LOCAL_DIR=".local"
+LOCAL_DIR=".local/protoc"
 mkdir -p "${LOCAL_DIR}"
 
 # install a specific version of protoc
-PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+export PATH="${LOCAL_DIR}/bin:$PATH"
 if ! protoc --version | grep "${PB_VERSION}" >/dev/null; then
-    if [[ ! -f "${LOCAL_DIR}/bin/protoc" ]]; then
-        curl -sLO "$PB_REL/download/v${PB_VERSION}/protoc-${PB_VERSION}-linux-x86_64.zip"
-        unzip "protoc-${PB_VERSION}-linux-x86_64.zip" -d "${LOCAL_DIR}"
-        rm "protoc-${PB_VERSION}-linux-x86_64.zip"
-    fi
-    export PATH="${LOCAL_DIR}/bin:$PATH"
+  if [[ -d "${LOCAL_DIR}" ]]; then
+    rm -rf "${LOCAL_DIR}"
+  fi
+  curl -sLO "https://github.com/protocolbuffers/protobuf/releases/download/v${PB_VERSION}/protoc-${PB_VERSION}-linux-x86_64.zip"
+  unzip "protoc-${PB_VERSION}-linux-x86_64.zip" -d "${LOCAL_DIR}"
+  rm "protoc-${PB_VERSION}-linux-x86_64.zip"
 fi
 
 # install a specific version of protoc-gen-go
@@ -30,13 +30,13 @@ go install "google.golang.org/protobuf/...@${PB_GO_VERSION}"
 ###
 
 if ! protoc --version | grep "${PB_VERSION}"; then
-    echo "protoc version is not ${PB_VERSION}"
-    exit 1
+  echo "protoc version is not ${PB_VERSION}"
+  exit 1
 fi
 
 if ! protoc-gen-go --version | grep "${PB_GO_VERSION}"; then
-    echo "protoc-gen-go version is not ${PB_GO_VERSION}"
-    exit 1
+  echo "protoc-gen-go version is not ${PB_GO_VERSION}"
+  exit 1
 fi
 
 ###
@@ -44,10 +44,10 @@ fi
 # Print commands
 set -x
 
-# Generate all protobuf code
-protoc -I"./proto" -I"$GOPATH/src" --go_out="$GOPATH/src" proto/*.proto
+# Generate Go code
+protoc -I"./proto" -I"$GOPATH/src" --go_out=. --go_opt=module=github.com/RoboCup-SSL/ssl-remote-control proto/*.proto
 
-# generate typescript code for frontend
+# Generate typescript code
 target_dir="./frontend/src/proto"
 mkdir -p "${target_dir}"
 protoc -I"./proto" \
